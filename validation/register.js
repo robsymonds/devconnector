67d23766723267bb1162348d8feba6d36cff16cf
Rob Symonds <rsymonds@gmail.com>
2018-08-03 16:07:39 -0400

    added validation to register, login

--- validation/register.js
+++ validation/register.js
@@ -4,8 +4,45 @@ const isEmpty = require('./is-empty');
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
-  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = 'Name must be between 2 and 3 characters.';
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.password2 = !isEmpty(data.password2) ? data.password2 : '';
+
+  if (
+    Validator.isEmpty(data.name) ||
+    !Validator.isLength(data.name, { min: 2, max: 30 })
+  ) {
+    errors.name = 'Name is required and must be between 2 and 3 characters.';
   }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email is required';
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (
+    Validator.isEmpty(data.password) ||
+    !Validator.isLength(data.password, { min: 6, max: 30 })
+  ) {
+    errors.password =
+      'Password is required and must be between 6 and 30 characters';
+  }
+
+  if (
+    Validator.isEmpty(data.password2) ||
+    !Validator.isLength(data.password2, { min: 6, max: 30 })
+  ) {
+    errors.password2 =
+      'Confirmation Password is required and must be between 6 and 30 characters';
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must match';
+  }
+
   return { errors, isValid: isEmpty(errors) };
 };
