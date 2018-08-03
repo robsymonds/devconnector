a0d1f52a8428dc708224b08d59e2a1a230c27179
Rob Symonds <rsymonds@gmail.com>
2018-08-03 16:12:06 -0400

    update keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb://robsymonds:robpassword1@ds125881.mlab.com:25881/rs_dev_connector',
-  secretOrKey: 'secret'
+    'mongodb://robsymonds:NNNNNN@ds125881.mlab.com:25881/rs_dev_connector',
+  secretOrKey: 'NNNNNN'
 };
